b66fcf98cc6b4812e4b64092b4b4e8c491cfe039
Meital <meiti2002@hotmail.com>
2024-12-30 15:41:52 -0300

    favico

--- Back/database/db.js
+++ Back/database/db.js
@@ -2,7 +2,7 @@ import {Sequelize} from "sequelize"
 
 //nombre de la db, user, contraseña, donde esta alojado, lenguaje, puerto//
 const db = new Sequelize("db_always", "root","",{
-    host: process.env.DB_HOST,
+    host: "localhost",
     dialect:"mysql",
     port:3306
 })
