5caaf9f278d81ac247afbc7a1ff908786818fc76
Meital <meiti2002@hotmail.com>
2025-01-03 15:00:30 -0300

    ..

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/"
+  base: "/", // Esto es necesario para que los recursos se carguen correctamente en producción
+  build: {
+    outDir: 'dist' // Asegúrate de que los archivos de salida se coloquen en la carpeta 'dist'
+  }
 })
