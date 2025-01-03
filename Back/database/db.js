234f282bd6518bec74e7c42ef4658a84381b0841
Meital <meiti2002@hotmail.com>
2025-01-03 14:28:06 -0300

    ..

--- Back/database/db.js
+++ Back/database/db.js
@@ -1,10 +1,11 @@
 import {Sequelize} from "sequelize"
-import mysql from "mysql"
+import mysql from "mysql2"
 
 //nombre de la db, user, contraseña, donde esta alojado, lenguaje, puerto//
 const db = new Sequelize("db_always", "root","",{
     host: "localhost",
-    dialect:"mysql",
+    dialect:"mysql2",
+    dialectModule: require('mysql2'),   
     port:3306
 })
 
