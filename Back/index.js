c98536e1e86dc3a33bf121f2485545606c140c49
Meital <meiti2002@hotmail.com>
2025-01-03 14:02:29 -0300

    ya tengo mysql

--- Back/index.js
+++ Back/index.js
@@ -15,8 +15,6 @@ import carrousel from "./Routes/carrousel.js";
 import path from "path";
 import { fileURLToPath } from 'url';
 
-import dotenv from "dotenv";
-dotenv.config();
 
 
 const __filename = fileURLToPath(import.meta.url);
