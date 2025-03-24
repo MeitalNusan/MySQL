90d7517b2f01d41ea7701d8a9e4965d2176ef2a3
Meital <meiti2002@hotmail.com>
2025-03-24 14:48:53 -0300

    probando23

--- Back/config.js
+++ Back/config.js
@@ -3,3 +3,4 @@ export const DB_USER = process.env.DB_USER || 'root'
 export const DB_PASSWORD = process.env.DB_PASSWORD || ""
 export const DB_NAME = process.env.DB_NAME || 'db_always' 
 export const DB_PORT = process.env.DB_PORT || 3306 
+export const MYSQL_PUBLIC_URL = process.env.MYSQL_PUBLIC_URL
--- Back/database/db.js
+++ Back/database/db.js
@@ -1,15 +1,16 @@
 import { Sequelize } from "sequelize";
 import mysql2 from "mysql2";
-import {DB_HOST, DB_NAME,DB_PASSWORD,DB_USER,DB_PORT} from "../config.js"
+import {DB_HOST, DB_NAME,DB_PASSWORD,DB_USER,DB_PORT, MYSQL_PUBLIC_URL} from "../config.js"
 
 
-const db = new Sequelize(process.env.MYSQL_PUBLIC_URL,{
+const db = new Sequelize("db_always", "root", "", {
   host:DB_HOST,
   user:DB_USER,
   password: DB_PASSWORD,
   port: DB_PORT,
   database: DB_NAME,
   dialect: "mysql",   
+  url: MYSQL_PUBLIC_URL
 });
 
 export default db;
