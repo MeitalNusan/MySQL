77c17690ce78b441f8270db02c9aa9f7ec21def3
Meital <meiti2002@hotmail.com>
2025-07-31 16:53:01 -0300

    ...

--- Back/database/db.js
+++ Back/database/db.js
@@ -12,12 +12,17 @@ const db = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: Number(process.env.DB_PORT),   
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
-)
-
+);
 export default db;
 
 
